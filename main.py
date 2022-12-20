c2b2332f608b768035b69852ecc466a89c9d89c7
mdallen5393 <allen_matthew@outlook.com>
2022-12-20 08:44:29 -0600

    removed main file

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import create, read, update, delete
-
-if __name__ == '__main__':
-    # add_bubble('test', 'test', 'test')
-    # print(read.get_bubble_resources('firestore'))
-    pass
